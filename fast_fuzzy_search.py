f6c59dfab0df4a41d177ec44b6cd13f4ade2108c
Ling Zhang <ling@aiden.ai>
2017-10-18 04:52:20 +0100

    Working code

--- fast_fuzzy_search.py
+++ fast_fuzzy_search.py
@@ -1,88 +1,18 @@
-from min_heap import MinHeap
-import phonetics
-from candidate import Candidate
+import pyphone
 from generate_candidates import generate_candidates
 
-def next_candidate(candidates):
-    if len(candidates) == 0:
-        return None
-    min_val = 1.6
-    best_candidate = None
-    for candidate in candidates.values():
-        if candidate.score == -1:
-            continue # This is blocked
-        if candidate.score < min_val:
-            best_candidate = candidate
-            min_val = candidate.score
-    candidates[best_candidate.ipa] = Candidate(-1, None) # Block this key for future
-    return best_candidate
-
-def fuzzy_search(word, dictionary, num_results=10):
+def fast_fuzzy_search(word, dictionary, num_results=10, max_distance=2):
     results = []
-    candidates = {}
-    phonetic_variants = phonetics.phoneticize(word)
-    print('checking', word, phonetic_variants)
-    input('ok?')
-
-    for phonetic in phonetic_variants:
-        candidate = Candidate(0, phonetic)
-        candidates[candidate.ipa] = candidate
-
-    while (len(candidates) > 0):
-        candidate = next_candidate(candidates)
-
-        result = dictionary.get(candidate.ipa)
-        if result:
-            results.append(result)
-
-        if len(results) == num_results:
-            break
-
-        # Cache results to avoid repetitions
-        for new_candidate in generate_candidates(candidate):
-            prev_candidate = candidates.get(new_candidate.ipa)
-            if prev_candidate is None or new_candidate < prev_candidate:
-                candidates[new_candidate.ipa] = new_candidate
-                print('adding', candidate, new_candidate, prev_candidate)
-        # print(candidate.ipa, min_heap._heap)
-
+    current_candidates = pyphone.phonex(word)
+    next_candidates = []
+    for i in range(max_distance):
+        for candidate in current_candidates:
+            if dictionary.get(candidate):
+                results.append(dictionary.get(candidate))
+            if len(results) == num_results:
+                return results
+            for new_candidate in generate_candidates(candidate):
+                next_candidates.append(new_candidate)
+        current_candidates = next_candidates
+        next_candidates = []
     return results
-
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-
-import scipy.cluster.hierarchy as hierarchy
-
-CLUSTER_SENSITIVITY = 0.5
-
-def cluster_phones(language):
-    phone_set = phonetics.phone_set(language)
-    vowels = list(filter(lambda x: phonetics.is_vowel(x), phone_set))
-    consonants = list(filter(lambda x: phonetics.is_vowel(x), phone_set))
-
-    phones = sorted(list(phonetics.phone_set(language)))
-    triangle_distance = []
-
-    for a in range(len(phones)):
-        for b in range(a + 1, len(phones)):
-            triangle_distance.append(phonetics.distance(phones[a], phones[b]))
-
-    linkages = hierarchy.linkage(triangle_distance)
-    clusters = hierarchy.fcluster(linkages, CLUSTER_SENSITIVITY)
-
-    forward_map = {}
-    cluster_map = {}
-
-    for index in range(len(clusters)):
-        set_phones = forward_map.get(clusters[index])
-        if not set_phones:
-            set_phones = []
-            forward_map[clusters[index]] = set_phones
-        set_phones.append(phones[index])
-        cluster_map[phones[index]] = clusters[index]
-
-    # To see the map
-    pp.pprint(forward_map)
-    print(len(phones))
-
-    return cluster_map
--- generate_candidates.py
+++ generate_candidates.py
@@ -1,31 +1,12 @@
-from candidate import Candidate
-import phonetics
+import pyphone
 
-
-PHONE_THRESHOLD = 0.3
-
-def generate_candidates(candidate, max_score=1.0, language="english"):
+def generate_candidates(candidate, language="english"):
     candidates = []
-    phones = phonetics.phone_set(language)
-
-    for position in range(len(candidate.ipa)):
-        current_phone = candidate.ipa[position]
-        # Deletion Edits
-        cost = 0.5 if phonetics.is_vowel(current_phone) else 0.5
-        new_candidate = Candidate(candidate.score + cost, candidate.ipa[:position] + candidate.ipa[position + 1:])
-        candidates.append(new_candidate)
-
+    phones = set(pyphone.cluster_phones(language).values())
+    for position in range(len(candidate)):
+        # Deletion deletes the current phone edits before the current_phone
+        candidates.append(candidate[:position] + candidate[position + 1:])
         for phone in phones:
             # Insertion edits before the current_phone
-            cost = 0.5 if phonetics.is_vowel(phone) else 0.5
-            new_candidate = Candidate(candidate.score + cost, candidate.ipa[:position] + (phone,) + candidate.ipa[position:])
-            candidates.append(new_candidate)
-            
-            # Substitution edits
-            cost = phonetics.distance(current_phone, phone)
-            if cost > 0.3:
-                continue
-            new_candidate = Candidate(candidate.score + cost, candidate.ipa[:position] + (phone,) + candidate.ipa[position + 1:])
-            candidates.append(new_candidate)
-
-    return filter(lambda x: x.score <= max_score, candidates)
+            candidates.append(candidate[:position] + (phone,) + candidate[position:])
+    return candidates
